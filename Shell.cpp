7aa72b0e3d29965ceafcba59c43085d3be885c5f
howhowlin <30687126+howhowlin@users.noreply.github.com>
2018-01-15 12:23:49 +0800

    Add files via upload

--- Shell.cpp
+++ Shell.cpp
@@ -17,28 +17,34 @@ int main( int argc , char **argv )
 {
   string input;
   string finalInput;
+  string result;
   cout << "?- ";
     while(getline (cin,input)){
         if(input=="halt."){
           break;
         }
-        else if(input.find(".", 0)!=string::npos){
-              finalInput=finalInput+input;
-              Scanner s(finalInput);
-              Parser p(s);
-              p.buildExpression();
-              string result = p.getExpressionResult();
-              std::cout << result << '\n';
-              finalInput="";
-              result="";
-              cout << "?- ";
-            }
         else if(input.length()==0){
           cout << "?- ";
         }
-        else{
-            finalInput=finalInput+input;
+        else if(input.find(";")!=-1&&input.find(".")==-1){
+          finalInput=finalInput+input;
           cout << "|   " ;
+        }
+        else{
+          finalInput=finalInput+" "+input;
+          Scanner s(finalInput);
+          Parser p(s);
+          try{
+          p.buildExpression();
+           result = p.getExpressionResult();
+        }
+        catch (std::string &msg) {
+           result = msg;// it should throw an exception like this.
+        }
+          std::cout << result << '\n';
+          finalInput="";
+          result="";
+          cout << "?- ";
         }
     }
 }
